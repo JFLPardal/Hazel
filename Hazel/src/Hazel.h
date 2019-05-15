42d3a9bde82f0d724d4af28a68fda1481c10bfd4
JoaoPardal <jflpardal@gmail.com>
2019-05-15 02:10:40 +0100

    Setup basic application and entry point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// used by applications made with Hazel
+// to make this work, in the application go to "Properties/ C/C++ / General / Additional Include Directories" and write "$(SolutionDir)Hazel\src;"
+
+#include <stdio.h>
+#include "Hazel/Application.h"
+
+// ENTRY POINT----------------------
+#include "Hazel/EntryPoint.h"
+// ---------------------------------
